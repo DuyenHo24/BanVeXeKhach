42df1eeda50cbf235956a543c9afa2ee36d57014
DuyenHo24 <73517656+DuyenHo24@users.noreply.github.com>
2020-11-25 19:44:16 +0700

    Add files via upload

---
+++ HTBanVe/src/main/java/com/mycompany/htbanve/TrangChuController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.htbanve;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS-LAPTOP
+ */
+public class TrangChuController implements Initializable {
+
+    @FXML
+    public void SwitchtoAddCX() throws IOException {
+        App.setRoot("AddChuyenXe");
+    }
+    @FXML
+    public void SwitchtoKH() throws IOException{
+        App.setRoot("Muave");
+    }
+    @FXML 
+    public void SwitchtoQLVX() throws IOException{
+        App.setRoot("QuanLyVeXe");
+    }
+    @FXML
+    public void ExitTC() throws IOException {
+        System.exit(0);
+    }
+        
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
