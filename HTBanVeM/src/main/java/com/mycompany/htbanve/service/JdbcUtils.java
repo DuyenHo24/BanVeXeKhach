1b5837ae45be892d0deb957eca482ad35bdc906a
1851010152tung <1851010152tung@ou.edu.vn>
2020-12-14 18:46:58 +0700

    Ban moi chinh sua

---
+++ HTBanVeM/src/main/java/com/mycompany/htbanve/service/JdbcUtils.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.htbanve.service;
+
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+
+
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ASUS-LAPTOP
+ */
+public class JdbcUtils {
+    private static Connection conn;
+      static
+      {        
+        try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/htbanve","root","123456789");
+            } catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, e);
+            }catch(SQLException ex){
+                Logger.getLogger(JdbcUtils.class.getName()).log(Level.SEVERE,null,ex);
+            }   
+    }  
+    public static Connection getConnection(){
+        return conn;
+    }
+    
+}
